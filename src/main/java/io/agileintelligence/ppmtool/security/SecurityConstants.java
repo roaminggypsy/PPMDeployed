96aeac934422a77c564e78d1bde49548000f3006
Linjun Ouyang <lynjunoyang@gmail.com>
2020-04-08 17:44:18 -0700

    changed token

--- src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
+++ src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
@@ -8,5 +8,5 @@ public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300000; // in milliseconds
+    public static final long EXPIRATION_TIME = 3000000; // in milliseconds
 }
