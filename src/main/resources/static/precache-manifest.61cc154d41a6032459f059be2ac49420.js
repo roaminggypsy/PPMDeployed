cef42085c34ad37aab3e84641c970f65f79e8b4b
Linjun Ouyang <lynjunoyang@gmail.com>
2020-04-08 19:30:40 -0700

    react

---
+++ src/main/resources/static/precache-manifest.61cc154d41a6032459f059be2ac49420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d6d736b237e1de8647e412cdd6ae84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a53964b6e7acbcdbac",
+    "url": "/static/css/2.8f4dfbca.chunk.css"
+  },
+  {
+    "revision": "2363ba0d92cefa2e02ef",
+    "url": "/static/css/main.87987d25.chunk.css"
+  },
+  {
+    "revision": "66a53964b6e7acbcdbac",
+    "url": "/static/js/2.bd8d24a3.chunk.js"
+  },
+  {
+    "revision": "1bf0dddb2358e2533b2cf14df6fbe8e9",
+    "url": "/static/js/2.bd8d24a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2363ba0d92cefa2e02ef",
+    "url": "/static/js/main.58e1ec30.chunk.js"
+  },
+  {
+    "revision": "c081e2f36df4981bab2b",
+    "url": "/static/js/runtime-main.8b4b340c.js"
+  }
+]);
